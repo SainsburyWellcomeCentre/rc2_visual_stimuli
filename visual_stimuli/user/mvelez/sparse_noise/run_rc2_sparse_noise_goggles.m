d3c976d7cf3c0e3647a67c8dec03f725fb711ee3
velezmat <m.velez-fort@ucl.ac.uk>
2026-06-09 10:38:32 +0100

    correction of path to load gamma table for wisecoco screens

--- visual_stimuli/user/mvelez/sparse_noise/run_rc2_sparse_noise_goggles.m
+++ visual_stimuli/user/mvelez/sparse_noise/run_rc2_sparse_noise_goggles.m
@@ -9,7 +9,7 @@ baseline_duration       = 10;        % s
 
 % distance_from_screen is now loaded automatically from setup config
 screen_name             = 'wisecoco';
-gamma_correction_file   = 'C:\Users\mateo\Documents\rc2\visual_stimuli\visual_stimuli\gamma\gamma_table_wisecoco.mat';
+gamma_correction_file   = 'C:\Users\mateo\Documents\rc2\visual_stimuli\gamma\gamma_table_wisecoco.mat';
 wait_for_start_trigger  = true;  % wait for start trigger, true or false
 
 % NI-DAQ info
